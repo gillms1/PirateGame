dfb7182223c2b46b5097e1d10ce7a589ccb2c28c
Sunny <sunny@Sunnys-MacBook-Pro.local>
2014-07-13 18:07:26 +0100

    Initial Commit

---
+++ PirateGame/MSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSGAppDelegate.h
+//  PirateGame
+//
+//  Created by Sunny on 13/07/2014.
+//  Copyright (c) 2014 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PirateGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PirateGame
+//
+//  Created by Sunny on 13/07/2014.
+//  Copyright (c) 2014 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSGAppDelegate class]));
+    }
+}
